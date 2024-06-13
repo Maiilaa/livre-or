a04abb3e13a9adc30be3fd5cc4f1338ea43e2004
Maiilaa <zirarimaila8@gmail.com>
2024-06-13 10:00:46 +0200

    10h00

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if (isset($_SESSION["login"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+    <link rel="stylesheet" href="livre-or.css">
 </head>
 <body>
     <header>
